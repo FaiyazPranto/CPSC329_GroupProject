f8f89b1353ecc0029cb53e4478f59e775e9ea245
FaiyazPranto <faiyazaltaf.pranto@ucalgary.ca>
2022-11-30 20:23:40 -0700

    .

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -1,9 +1,8 @@
 package application;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
+import java.awt.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.*;
 
@@ -22,8 +21,15 @@ import javafx.stage.Stage;
 
 public class GameController {
 	
-	public String string8[][] = new String[20][8];
+	public static String[][] string8 = new String[20][8];
+    int l;
+    String file_p = "src/application/Level%dQ.txt";
+    String line;
+    int j = 0;
 	Stage applicationStage;
+    int lvl;
+    public static int o_point = 0;
+    String points = "Total Score: %d/320";
 	
 	@FXML
     private Button level5;
@@ -51,9 +57,10 @@ public class GameController {
 
     @FXML
     private Label label6;
+    
 
     @FXML
-    private Label main_label;
+    private static Label main_label;
 
     @FXML
     private Button level1;
@@ -93,20 +100,49 @@ public class GameController {
 
     @FXML
     private Label Question_label;
+    
+    @FXML
+    private	Button update_button;
+
+    @FXML
+    void update(ActionEvent event) {
+    	String update_message = String.format("%d/320", o_point);
+    	main_label.setText(update_message); 
+    }
+    
+    public GameController() throws FileNotFoundException {
+        this.lvl = lvl;
+        this.string8 = string8;
+        this.o_point = o_point;
+    }
 
     
     @FXML
-    void openLevel1(ActionEvent level1Start) throws Exception {
+    void openLevel8(ActionEvent level1Start) throws Exception {
+        lvl = 1;
+        l = 8;
 
+        File quest_file = new File(String.format(file_p, l));
+        BufferedReader br = new BufferedReader(new FileReader(quest_file));
+        while ((line = br.readLine()) != null) {
+            String[] question_array = line.split(",");
+            string8[j] = question_array;
+            j++;
+        }
+        String path = String.format("src/application/Level%d.fxml", lvl);
+        String title = String.format("Level %d", lvl);
     	FXMLLoader fxmlLoader = new FXMLLoader();
-        VBox child1 = fxmlLoader.load(new FileInputStream("src/application/Level1.fxml"));
-        Scene scene1 = new Scene(child1, 600,400);
-        
-        
+        VBox child1 = fxmlLoader.load(new FileInputStream(path));
+        Scene scene1 = new Scene(child1, 700,600);
+
+
 		Stage secondaryStage = new Stage();
-		secondaryStage.setTitle("Level 1");
+		secondaryStage.setTitle(title);
 		secondaryStage.setScene(scene1);
+
+
 		secondaryStage.show();
+
     }
 
     private Object quizAnswers(Scene level1Scene) {
@@ -147,13 +183,12 @@ public class GameController {
     }
     
     @FXML
-    void openLevel8(ActionEvent event) throws IOException, IOException {
+    void openLevel1(ActionEvent event) throws IOException, IOException {
 
     	FXMLLoader fxmlLoader = new FXMLLoader();
         VBox child1 = fxmlLoader.load(new FileInputStream("src/application/Level1.fxml"));
         Scene scene1 = new Scene(child1, 700, 600);
-        
-        
+
 		Stage secondaryStage = new Stage();
 		secondaryStage.setTitle("Level 1");
 		secondaryStage.setScene(scene1);
@@ -177,6 +212,16 @@ public class GameController {
     void give_hint(ActionEvent event) {
 
     }
+
+    public static String[][] get_array(){
+        return string8;
+    }
+
+    public static void seto_point(int point){
+        o_point += point;
+        String k = String.format("%d",o_point);
+    }
+
 
 
 
--- src/application/LevelsController.java
+++ src/application/LevelsController.java
@@ -1,140 +1,167 @@
 package application;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 
-public class LevelsController {
-	
-	public String string8[][] = new String[20][8];
-	 @FXML
-	    private Button Submit_button;
+public class LevelsController<option1> {
+	int a = 12;
 
-	    @FXML
-	    private TextArea Hint_box;
+	public String string8[][] = GameController.get_array();
+	/**
+	 * File quest_file = new File("src/application/Level8Q.txt");
+	 * BufferedReader br = new BufferedReader(new FileReader(quest_file));
+	 * String line;
+	 * int j = 0;
+	 **/
+	@FXML
+	private Button Submit_button;
 
-	    @FXML
-	    private CheckBox option3;
+	@FXML
+	private TextArea Hint_box;
 
-	    @FXML
-	    private Button Next_button;
+	@FXML
+	private CheckBox option3;
 
-	    @FXML
-	    private CheckBox option4;
+	@FXML
+	private Button Next_button;
 
-	    @FXML
-	    private Button Hint_button;
+	@FXML
+	private CheckBox option4;
 
-	    @FXML
-	    private CheckBox option1;
+	@FXML
+	private Button Hint_button;
 
-	    @FXML
-	    private CheckBox option2;
+	@FXML
+	private CheckBox option1;
 
-	    @FXML
-	    private Label Question_label;
-	    
-	    int i = 0;
-	    @FXML
-	    void go_next(ActionEvent event) throws FileNotFoundException {
-	    	if(option1.isSelected() || option2.isSelected() || option3.isSelected() || option4.isSelected()) {
-	    	Hint_box.clear();
-	    	
-	    	if(i == 0) {
-	   
-	    		File quest_file = new File("src/application/Level8Q.txt");
-	    		Scanner file_reader = new Scanner(quest_file);
-	    		int j = 0;
-	    		while(file_reader.hasNextLine()) {
-	    			String question = file_reader.nextLine();
-	    			String[] question_array = question.split(",");
-	    			string8[j] = question_array;
-	    			j++;
-	    		}
-	    		Question_label.setText(string8[0][0]);
-	    		option1.setText(string8[0][1]);
-	    		option2.setText(string8[0][2]);
-	    		option3.setText(string8[0][3]);
-	    		option4.setText(string8[0][4]);
-	    		i++;
-	    	}
-	    	else if(i < string8.length){
-	    		Question_label.setText(string8[i][0]);
-		    	option1.setText(string8[i][1]);
-		    	option2.setText(string8[i][2]);
-		    	option3.setText(string8[i][3]);
-		    	option4.setText(string8[i][4]);
-		    	i++;
-	    			
-	    		}
-	    	}
-	    	else {
-	    		Hint_box.setText("Please Select an option!!");
-	    	}
-	    	
-	  
-	    	}
-	    
-	    
+	@FXML
+	private CheckBox option2;
 
-	    @FXML
-	    void validate(ActionEvent event) {
-	    	if(option1.isSelected() == false && option2.isSelected() == false && option3.isSelected() == false && option4.isSelected() == false) {
-	    		Hint_box.setText("please select an option to validate!!");
-	    		
-	    	}
-	    	else if((option1.isSelected() && option2.isSelected()) || (option1.isSelected() && option3.isSelected()) || (option1.isSelected() && option4.isSelected()) || (option2.isSelected() && option3.isSelected()) || (option2.isSelected() && option4.isSelected()) || (option3.isSelected() && option4.isSelected())) {
-	    		Hint_box.setText("Please choose one option only!!");
-	    	}
-	    	else {
-	    		String correct = string8[i-1][5];
-	    		if(option1.isSelected()) {
-	    			if(option1.getText().equals(correct)) {
-	    				Hint_box.setText("CORRECT!!\n"+string8[i-1][6]);
-	    			}
-	    			else {
-	    				Hint_box.setText("WRONG!!\n"+string8[i-1][6]);
-	    			}
-	    		}
-	    		if(option2.isSelected()) {
-	    			if(option2.getText().equals(correct)) {
-	    				Hint_box.setText("CORRECT!!\n"+string8[i-1][6]);
-	    			}
-	    			else {
-	    				Hint_box.setText("WRONG!!\n"+string8[i-1][6]);
-	    			}
-	    		}
-	    		if(option3.isSelected()) {
-	    			if(option3.getText().equals(correct)) {
-	    				Hint_box.setText("CORRECT!!\n"+string8[i-1][6]);
-	    			}
-	    			else {
-	    				Hint_box.setText("WRONG!!\n"+string8[i-1][6]);
-	    			}
-	    		}
-	    		if(option4.isSelected()) {
-	    			if(option4.getText().equals(correct)) {
-	    				Hint_box.setText("CORRECT!!\n"+string8[i-1][6]);
-	    			}
-	    			else {
-	    				Hint_box.setText("WRONG!!\n"+string8[i-1][6]);
-	    			}
-	    		}
-	    	}
-	    }
+	@FXML
+	private Label Question_label;
 
-	    @FXML
-	    void give_hint(ActionEvent event) {
+	@FXML
+	private ProgressBar progressBar;
 
-	    }
+	int i = 1;
+	boolean check = false;
+	int q_num = 1;
+	static int level_point=0;
+
+	static int get_point() {
+		return level_point;
+	}
+
+
+	public LevelsController() throws FileNotFoundException {
+
+	}
+
+	@FXML
+	void go_next(ActionEvent event) throws IOException {
+		if (check == true) {
+			//if (option1.isSelected() || option2.isSelected() || option3.isSelected() || option4.isSelected()) {
+				Hint_box.clear();
+
+				/**
+				 while ((line = br.readLine()) != null) {
+				 String[] question_array = line.split(",");
+				 string8[j] = question_array;
+				 j++;
+				 }
+				 **/
+
+				if (i < string8.length) {
+					Question_label.setText(string8[i][0]);
+					option1.setText(string8[i][1]);
+					option2.setText(string8[i][2]);
+					option3.setText(string8[i][3]);
+					option4.setText(string8[i][4]);
+					i++;
+					if(i == string8.length){
+						Next_button.setText("End");
+					}
+
+				}else{
+					GameController.seto_point(a);
+				}
+
+				check = false;
+				q_num++;
+				//progressBar.setProgress(q_num/(string8.length-1));
+
+				option1.setSelected(false);
+				option2.setSelected(false);
+				option3.setSelected(false);
+				option4.setSelected(false);
+
+
+			 /*else {
+				Hint_box.setText("Please Select an option!!");
+			}*/
+		} else {
+			Hint_box.setText("Please click submit first!!");
+		}
+
+	}
+
+
+	@FXML
+	void validate(ActionEvent event) {
+		if (option1.isSelected() == false && option2.isSelected() == false && option3.isSelected() == false && option4.isSelected() == false) {
+			Hint_box.setText("please select an option to validate!!");
+
+		} else if ((option1.isSelected() && option2.isSelected()) || (option1.isSelected() && option3.isSelected()) || (option1.isSelected() && option4.isSelected()) || (option2.isSelected() && option3.isSelected()) || (option2.isSelected() && option4.isSelected()) || (option3.isSelected() && option4.isSelected())) {
+			Hint_box.setText("Please choose one option only!!");
+		} else if (check == false){
+			String correct = string8[i-1][5];
+			if (option1.isSelected()) {
+				if (option1.getText().equals(correct)) {
+					Hint_box.setText("CORRECT!!\n" + string8[i - 1][6]);
+					level_point+=2;
+				} else {
+					Hint_box.setText("WRONG!!\n" + string8[i - 1][6]);
+				}
+			}
+			if (option2.isSelected()) {
+				if (option2.getText().equals(correct)) {
+					Hint_box.setText("CORRECT!!\n" + string8[i-1][6]);
+					level_point+=2;
+				} else {
+					Hint_box.setText("WRONG!!\n" + string8[i - 1][6]);
+				}
+			}
+			if (option3.isSelected()) {
+				if (option3.getText().equals(correct)) {
+					Hint_box.setText("CORRECT!!\n" + string8[i - 1][6]);
+					level_point+=2;
+				} else {
+					Hint_box.setText("WRONG!!\n" + string8[i - 1][6]);
+				}
+			}
+			if (option4.isSelected()) {
+				if (option4.getText().equals(correct)) {
+					Hint_box.setText("CORRECT!!\n" + string8[i - 1][6]);
+					level_point+=2;
+				} else {
+					Hint_box.setText("WRONG!!\n" + string8[i - 1][6]);
+				}
+			}
+			check = true;
+		}
+	}
+
+	@FXML
+	void give_hint(ActionEvent event) {
+
+	}
 
 }
+
